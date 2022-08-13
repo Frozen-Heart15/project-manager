0dc145938f2a0ddb4d9f48079c865d0d2b218025
gaurav choudhary <choudharyg015@gmail.com>
2022-08-13 22:08:54 +0530

    created full stack app

---
+++ server/sampleData.js
@@ -0,0 +1,72 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, magnam neque ut enim sunt omnis. Est nobis officia corporis praesentium?',
+    status: 'In progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Dating App',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, magnam neque ut enim sunt omnis. Est nobis officia corporis praesentium?',
+    status: 'In progress',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'Business Website',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, magnam neque ut enim sunt omnis. Est nobis officia corporis praesentium?',
+    status: 'done',
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Course Website',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, magnam neque ut enim sunt omnis. Est nobis officia corporis praesentium?',
+    status: 'In progress',
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Media Services Website',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, magnam neque ut enim sunt omnis. Est nobis officia corporis praesentium?',
+    status: 'In progress',
+  },        
+];
+
+const clients = [
+  {
+    id: '1',
+    name: 'Tony Stark',
+    email: 'tony@gmail.com',
+    phone: '343-567-8901',
+  },
+  {
+    id: '2',
+    name: 'Steve Rogers',
+    email: 'steve@gmail.com',
+    phone: '343-567-8901',
+  },  
+  {
+    id: '3',
+    name: 'Natasha Romanoff',
+    email: 'natasha@gmail.com',
+    phone: '343-567-8901',
+  },
+  {
+    id: '4',
+    name: 'Thor Odinson',
+    email: 'thor@gmail.com',
+    phone: '343-567-8901',
+  },  
+  {
+    id: '5',
+    name: 'Bruce Banner',
+    email: 'bruce@gmail.com',
+    phone: '343-567-8901',
+  },    
+];
+
+module.exports = { projects, clients };
